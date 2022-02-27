0a54243a4157f9df02b7468491aa0fdcb8494f0b
Emily3403 <seebeckemily3403@gmail.com>
2022-02-28 00:34:34 +0100

    Modified: bootstrap/decrypt.sh

--- bootstrap/decrypt.sh
+++ bootstrap/decrypt.sh
@@ -10,5 +10,10 @@ if [ ! -f "pubkey.asc" ];  then
     exit 1
 fi
 
+while read line; do
+  echo "reading: ${line}"
+done < /dev/stdin
+
+exit 1
 
 zbarimg -1 --raw -q -Sbinary secret.png  | openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 | paperkey --pubring pubkey.asc | gpg --import
