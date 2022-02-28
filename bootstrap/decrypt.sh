7bb0ed75aba0d2c622df95641337ab3b29dac452
Emily3403 <seebeckemily3403@gmail.com>
2022-02-28 01:00:27 +0100

    Modified: bootstrap/decrypt.sh

--- bootstrap/decrypt.sh
+++ bootstrap/decrypt.sh
@@ -10,4 +10,6 @@ if [ ! -f "pubkey.asc" ];  then
     exit 1
 fi
 
-zbarimg -1 --raw -q -Sbinary secret.png  | openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 | paperkey --pubring pubkey.asc | gpg --import
+read -p "Password please: " -s password
+
+zbarimg -1 --raw -q -Sbinary secret.png  | openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -pass file:<(echo -n $password) | paperkey --pubring pubkey.asc | gpg --import
